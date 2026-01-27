5ded665974ec9949cd3d2a2ba2aa49bd6549ce4a
LucianoCatapan <lcatapan@gmail.com>
2026-01-27 16:46:12 -0300

    first commit initial project

---
+++ calculoImplicito/src/main/java/com/luciano/calculoImplicito/CalculoImplicitoApplication.java
@@ -0,0 +1,13 @@
+package com.luciano.calculoImplicito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculoImplicitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculoImplicitoApplication.class, args);
+	}
+
+}
---
+++ calculoImplicito/src/test/java/com/luciano/calculoImplicito/CalculoImplicitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luciano.calculoImplicito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculoImplicitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
