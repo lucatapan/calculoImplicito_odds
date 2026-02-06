8a0abbe32bfc3781043fbdf4ce2c08c629eb5590
Luciano Catapan <lcatapan@gmai.com>
2026-02-06 16:00:55 -0300

    --commit 3 aprendendo get and path

--- calculoImplicito/src/main/java/com/luciano/calculoImplicito/controller/BinomialController.java
+++ calculoImplicito/src/main/java/com/luciano/calculoImplicito/controller/BinomialController.java
@@ -1,14 +1,14 @@
 package com.luciano.calculoImplicito.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class BinomialController {
 
-    @GetMapping("/binomial")
-    public String calcular(@RequestParam int n, @RequestParam int k) {
+    @GetMapping("/binomial/{n}/{k}")
+    public String calcular(@PathVariable int n, @PathVariable int k) {
         return "Coeficiente binomial C(" + n + "," + k + ") = " + binomial(n, k);
     }
 
