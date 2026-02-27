2fc55ff2deea679396b0b2a8bc02f335b1d15871
Luciano Catapan <lcatapan@gmai.com>
2026-02-26 21:25:49 -0300

    correção aritimética

--- calculoImplicito/src/main/java/com/luciano/calculoImplicito/controller/BinomialController.java
+++ calculoImplicito/src/main/java/com/luciano/calculoImplicito/controller/BinomialController.java
@@ -1,5 +1,6 @@
 package com.luciano.calculoImplicito.controller;
 
+import org.apache.commons.math3.util.CombinatoricsUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,18 +10,14 @@ public class BinomialController {
 
     @GetMapping("/binomial/{n}/{k}")
     public String calcular(@PathVariable int n, @PathVariable int k) {
-        return "Coeficiente binomial C(" + n + "," + k + ") = " + binomial(n, k);
-    }
-
-    private long binomial(int n, int k) {
-        return fatorial(n) / (fatorial(k) * fatorial(n - k));
-    }
-
-    private long fatorial(int num) {
-        long resultado = 1;
-        for (int i = 2; i <= num; i++) {
-            resultado *= i;
+        if (k < 0 || k > n || n < 0) {
+            return "Erro: parâmetros inválidos (k deve estar entre 0 e n, n >= 0)";
         }
-        return resultado;
+        try {
+            long coef = CombinatoricsUtils.binomialCoefficient(n, k);
+            return "Coeficiente binomial C(" + n + "," + k + ") = " + coef;
+        } catch (ArithmeticException e) {
+            return "Erro: valor muito grande para calcular com precisão (n muito alto)";
+        }
     }
 }
